f7fb6c44e651437aa21aeb8fc6931d60b2433bad
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-03-17 14:14:40 -0300

    Refactor Pum class into Estudante with new fields

--- pum/src/main/java/com/mycompany/pum/Pum.java
+++ pum/src/main/java/com/mycompany/pum/Pum.java
@@ -1,34 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+public class Estudante {
 
-package com.mycompany.pum;
+    private int codMatricula;
+    private String semestreIngresso;
+    private int anosIngresso;
+    private String situacaoAcademica;
+    private static String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    private String telefone;
+    public String digito;
 
-import java.util.Date;
+    private int obterIdade(Date hoje) {
+        int idade = 0;
+        return idade;
+    }
 
-class Pum {
-   
-        private static String nome;
-        private Date dataNascimento;
-        private String cpf;
-        private char genero;
-        private String matricula;
-        private String email;
-        
-        private int obterIdade(Date hoje) {
-            int idade = 0;
-            return idade;
-        }
-        public Pum() {
-            nome = "Nobody";
-        }
-        
-        public static String getNome(){
-            return nome;
-        }
-        
-        public static void setNome(String _nome) {
-            nome = _nome;
-        }
+    public Estudante() {
+        nome = "Nobody";
+    }
+
+    public static String getNome() {
+        return nome;
+    }
+
+    public static void setNome(String _nome) {
+        nome = _nome;
+    }
+    public void setMatricula(String _digito) {
+        digito = _digito;
+    }
+    public String getMatricula() {
+        return digito = matricula+semestreIngresso+digito;
+    }
     
 }
+
