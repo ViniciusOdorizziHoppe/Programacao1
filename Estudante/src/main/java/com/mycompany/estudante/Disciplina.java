ec2bb9a196ee82b41b86993cbe43ce780196e16b
aluno <aluno@lab01-16>
2026-03-23 08:44:32 -0300

    Novas alterações

---
+++ Estudante/src/main/java/com/mycompany/estudante/Disciplina.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estudante;
+
+/**
+ *
+ * @author aluno
+ */
+public class Disciplina {
+
+    private String codigo;
+    private String nome;
+    private int cargaHoraria;
+    private String ementa;
+    private String objetivos;
+    private String referencias;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public String getOnbjetivos() {
+        return objetivos;
+    }
+
+    public String getReferencias() {
+        return referencias;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public void setOnbjetivos(String onbjetivos) {
+        this.objetivos = onbjetivos;
+    }
+
+    public void setReferencias(String referencias) {
+        this.referencias = referencias;
+    }
+    
+    
+}
