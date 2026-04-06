08f018e22d3c3724f77007186d6b128c5cf5c7d6
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-04-06 09:12:11 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.no;
+
+/**
+ *
+ * @author aluno
+ */
+public class No {
+
+    private String nome;
+    private int codigo;
+    private No prox;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+    
+        
+}
